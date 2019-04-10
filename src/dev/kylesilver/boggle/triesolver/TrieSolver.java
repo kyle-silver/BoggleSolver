dcabb746acb4373248c80ab216e225d8670c00a7
kyle <kyleAsilver@gmail.com>
2019-04-10 13:08:05 -0400

    refactored Board and Tile API --- A more efficient way to find the neigbors of a given Tile is to store all of the neighbors as a set inside the Tile class. Then when we compute the unvisited neighbors of a tile, we can just iterate over that set and check the hashes against the "visited" set in Board.

--- src/dev/kylesilver/boggle/triesolver/TrieSolver.java
+++ src/dev/kylesilver/boggle/triesolver/TrieSolver.java
@@ -17,28 +17,17 @@ public class TrieSolver {
 
     public Set<String> findAllWords(Board board) {
         Set<String> foundWords = new HashSet<>();
-        for (int row = 0; row < board.numRows(); row++) {
-            for (int col = 0; col < board.numCols(); col++) {
-                searchFrom(new Tile(row, col), board, foundWords);
-            }
+        for (Tile tile : board.tiles()) {
+        	searchFrom(tile, board, root, foundWords);
         }
         return foundWords;
-    }
-
-    private void searchFrom(Tile tile, Board board, Set<String> foundWords) {
-        board.clearVisited();
-        if (root.containsKey(board.charAt(tile))) {
-            board.addToVisited(tile);
-            searchFrom(tile, board, root.getChild(board.charAt(tile)), foundWords);
-        }
     }
 
     private void searchFrom(Tile tile, Board board, TrieNode node, Set<String> foundWords) {
         Set<Tile> neighbors = board.unvisitedNeighbors(tile);
         for (Tile neighbor : neighbors) {
-            char letter = board.charAt(neighbor);
-            if (node.containsKey(letter)) {
-                TrieNode next = node.getChild(letter);
+            if (node.containsKey(tile.value())) {
+                TrieNode next = node.getChild(tile.value());
                 if (next.isTerminator()) {
                     foundWords.add(next.getWord());
                 }
