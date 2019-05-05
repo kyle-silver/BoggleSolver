627278d4e78f3bd22cdc16f3175507cfc27adb25
Kyle Silver <kyle@Kyles-MBP.hsd1.nj.comcast.net>
2019-05-05 19:11:33 -0400

    command line support
    The app can now accept an arbitrary dict and an arbitrary directory
    of boggle boards, and defaults to the res folder if no args are
    provided.
    
    $Boggle.java <dictionary_path> <board_dir_path>

--- src/dev/kylesilver/boggle/Boggle.java
+++ src/dev/kylesilver/boggle/Boggle.java
@@ -19,14 +19,30 @@ public class Boggle {
     private static final String DICTIONARY_PATH = "./res/dictionary.txt";
     private static final String BOARDS_PATH     = "./res/boards/";
 
-    public static void main(String[] args) {
-        List<String> dictionary = FileReader.getDictionary(DICTIONARY_PATH);
-        List<Board> boards = loadBoards(BOARDS_PATH);
+    public static void main(String[] args) throws IOException {
+        String dictPath = setDictionaryPath(args);
+        String boardsPath = setBoardsPath(args);
+
+        List<String> dictionary = FileReader.getDictionary(dictPath);
+        List<Board> boards = loadBoards(boardsPath);
         
         BoggleSolver trieSolver = new TrieSolver(dictionary);
         
         boards.forEach(board -> timedSolve(trieSolver, board));
-        
+    }
+
+    private static String setDictionaryPath(String[] args) {
+        if (args.length == 2) {
+            return args[1];
+        }
+        return DICTIONARY_PATH;
+    }
+
+    private static String setBoardsPath(String[] args) {
+        if (args.length == 2) {
+            return args[0];
+        }
+        return BOARDS_PATH;
     }
     
     private static Set<String> timedSolve(BoggleSolver solver, Board board) {
@@ -37,7 +53,7 @@ public class Boggle {
         return result;
     }
     
-    private static List<Board> loadBoards(String boardDirectoryPath) {
+    private static List<Board> loadBoards(String boardDirectoryPath) throws IOException {
         List<Board> boards = new ArrayList<>();
         try (Stream<Path> paths = Files.walk(Paths.get(boardDirectoryPath))) {
             paths
@@ -48,6 +64,7 @@ public class Boggle {
                 });
         } catch (IOException ioe) {
             System.out.println("Path to boards directory was invalid");
+            throw ioe;
         }
         return boards;
     }
